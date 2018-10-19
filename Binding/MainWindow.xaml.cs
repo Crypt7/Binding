d9b6661d3f2e9b2fc086af6ab3c1b6c01c632dda
Vitalii.Rogozhyn <vitalii.rogozhyn@gmail.com>
2018-10-20 00:32:44 +0300

    03.INPC Demo

--- Binding/MainWindow.xaml.cs
+++ Binding/MainWindow.xaml.cs
@@ -20,10 +20,24 @@ namespace Binding
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Employee emp;
+        //private Employee emp1;
         public MainWindow()
         {
             InitializeComponent();
-            DataContext = Employee.GetEmploye();
+            emp = new Employee
+            {
+                Name = "Joe",
+                Title = "Qa"
+            };
+            DataContext = emp;
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            emp.Name = "Jessie";
+            emp.Title = "Dev";
+                
         }
     }
 }
--- Binding/Properties/Employee.cs
+++ Binding/Properties/Employee.cs
@@ -1,24 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Binding
 {
-    public class Employee
+    public class Employee : INotifyPropertyChanged
     {
-        public string Name { get; set; }
-        public string Title { get; set; }
-
-        public static Employee GetEmploye()
+        private string name;
+    public string Name
         {
-            var emp = new Employee
+         get
+            { return name; }
+         set
             {
-                Name = "jack",
-                Title = "Developer"
-            };
-            return emp;
+                name = value;
+                OnPropertyChanged();
+            }
+        }
+        private string title;
+
+        public Employee()
+        {
+        }
+
+        public string Title
+        {
+            get
+            { return title; }
+            set
+            {
+                title = value;
+                OnPropertyChanged();
+            }
+        }
+
+    public static Employee GetEmploye()
+    {
+        var emp = new Employee
+        {
+            Name = "jack",
+            Title = "Developer"
+        };
+        return emp;
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    private void OnPropertyChanged(
+        [CallerMemberName] string caller = "")
+    {
+        if (PropertyChanged != null)
+        {
+                PropertyChanged(this, new PropertyChangedEventArgs(caller));
         }
     }
 }
+}
+    
+
